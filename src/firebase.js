e73b0866c3d981c09cf992a993b37f86ee777417
Daniele Maggiore <dmaggiore@sorint.it>
2022-05-19 17:29:42 +0200

    Firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCAsDNhSczri3zCQDz2bFXV1CQ8cmCx87I',
+  authDomain: 'danielemaggiore-f6492.firebaseapp.com',
+  projectId: 'danielemaggiore-f6492',
+  storageBucket: 'danielemaggiore-f6492.appspot.com',
+  messagingSenderId: '988790233039',
+  appId: '1:988790233039:web:fc2ef1b28860ab714e64ba',
+  site: 'danielemaggiore',
+  public: 'public',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
