da89323a207f7947aa60bf093c8550e01105e791
Daniele Maggiore <dmaggiore@sorint.it>
2022-05-27 08:39:51 +0200

    Firebase hosting - first deploy

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMxnVzAjUrSSZaGpA3AxbwX9_Jhq9GkXU",
+  authDomain: "dm-portfolio-7bd5c.firebaseapp.com",
+  projectId: "dm-portfolio-7bd5c",
+  storageBucket: "dm-portfolio-7bd5c.appspot.com",
+  messagingSenderId: "296020851283",
+  appId: "1:296020851283:web:cf61c18ce923bd86e424a6"
+};
+
+const app = initializeApp(firebaseConfig);
